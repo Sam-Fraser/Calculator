691f6bbe2be61efec1d124f3a99f006fc361d1c2
Sam Eff <HeySamEff@gmail.com>
2020-05-13 17:39:11 -0700

    created function to display numbers 'displayNum'

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let firstOperand = 0;
-let secondOperand = 0;
-let answer = 0;
+let currentOperand = '';
+let currentAnswer = '';
+const operands = [];
+const operations = [];
 
 function add(a,b) {
     a = Number(a);
@@ -45,9 +46,54 @@ function operate(operation) {
 }
 
 function displayNum() {
-    
+    let displayText = document.getElementById('displayText');
+
+    const nums = document.querySelectorAll('#numbers button');
+    console.table(nums);
+    nums.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            switch (button.getAttribute('id')) {
+                case 'one':
+                    currentOperand += '1';
+                    break;
+                case 'two':
+                    currentOperand += '2';
+                    break;
+                case 'three':
+                    currentOperand += '3';
+                    break;
+                case 'four':
+                    currentOperand += '4';
+                    break;
+                case 'five':
+                    currentOperand += '5';
+                    break;
+                case 'six':
+                    currentOperand += '6';
+                    break;
+                case 'seven':
+                    currentOperand += '7';
+                    break;
+                case 'eight':
+                    currentOperand += '8';
+                    break;        
+                case 'nine':
+                    currentOperand += '9';
+                    break;
+                case 'zero':
+                    currentOperand += '0';
+                    break;
+                default:
+                    currentOperand = answer;
+            }
+            displayText.textContent = currentOperand;
+        });
+    });
+
 }
 
-function getFirstOperand(num) {
+function getOperand() {
 
 }
+
+displayNum();
