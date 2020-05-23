886be314127725fa920e86dc4bc078feebc2c75d
Sam Eff <HeySamEff@gmail.com>
2020-05-23 12:41:18 -0700

    rewrote code from bottom commented out code to top code.

--- script.js
+++ script.js
@@ -1,4 +1,126 @@
-let currentOperand = '';
+let operator
+let firstNum = '';
+let secondNum = '';
+
+let displayText = document.getElementById('displayText');
+
+const nums = document.querySelectorAll('#numbers button');
+const operators = document.querySelectorAll('#operators button');
+const clear = document.getElementById('clear');
+
+nums.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (operator == null) {
+            if (!(e.target.getAttribute('class') === 'inUse')) {
+                firstNum += button.textContent;
+                displayText.textContent = firstNum;
+            }
+        } else {
+            if (!(e.target.getAttribute('class') === 'inUse')) {
+                secondNum += button.textContent;
+                displayText.textContent = secondNum;
+            }
+        }
+
+        if (button.getAttribute('id') === 'decimal') {
+            e.target.classList.add('inUse');
+        }
+    });
+});
+
+operators.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        for (let i=0; i<operators.length; i++) {
+            if (operators[i].getAttribute('class') === 'active') {
+                operators[i].classList.remove('active');
+            } else {
+                continue;
+            }
+        }
+        e.target.classList.add('active');
+        if (operator == null || operator === 'equals') {
+            operator = button.getAttribute('id');
+            document.getElementById('decimal').classList.remove('inUse');
+        } else if (button.getAttribute('id') === 'clear') {
+            document.getElementById('decimal').classList.remove('inUse');
+        } else {
+            operate(operator, firstNum, secondNum);
+            operator = button.getAttribute('id');
+            document.getElementById('decimal').classList.remove('inUse');
+        }
+    });
+});
+
+function operate(op, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch(op) {
+        case 'add':
+            firstNum = add(num1,num2);
+            secondNum = '';
+            break;
+        case 'subtract':
+            firstNum = subtract(num1,num2);
+            secondNum = '';
+            break;
+        case 'multiply':
+            firstNum = multiply(num1,num2);
+            secondNum = '';
+            break;
+        case 'divide':
+            firstNum = divide(num1,num2);
+            secondNum = '';
+            break;
+        default:
+            break;
+    }
+    
+    displayText.textContent = firstNum;
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    if (b===0) {
+        return 'No No No No No No';
+    } else {
+        return a/b;
+    }
+}
+
+clear.addEventListener('click', (e) => {
+    operator = undefined;
+    firstNum = '';
+    secondNum = '';
+    displayText.textContent = '';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let currentOperand = '';
 let previousOperand = '';
 let answer = '';
 let currentOperator = '';
@@ -108,6 +230,7 @@ function operate() {
                 answer = '';
                 displayText.textContent = '';
             } else {
+                getAnswer(currentOperator);
                 currentOperator = e.target.getAttribute('id');
                 currentOperand = '';
             }
@@ -136,3 +259,4 @@ function getAnswer(operator) {
 }
 
 displayValue();
+*/
